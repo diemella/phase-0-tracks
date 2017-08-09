7823e4a0714981c6bbe8e3735d5118fbabb0fde1
Diem Hoang <hoangd5@gmail.com>
2017-08-09 15:04:20 -0500

    Add code segment to ask and evaluate age

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,3 +4,29 @@ puts "What is your name?"
 employee_name = gets.chomp
 employee_name = employee_name.downcase
 puts "Response: #{employee_name}"
+
+# Age Code Block
+
+puts "How old are you?"
+employee_age_given = gets.chomp
+employee_age_given = employee_age_given.to_i
+puts "Response: #{employee_age_given}"
+
+puts "What year were you born?"
+employee_birth_year = gets.chomp
+employee_birth_year = employee_birth_year.to_i
+puts "Response: #{employee_birth_year}"
+
+current_year = 2017
+employee_age_calculated = current_year - employee_birth_year
+puts "Calculated age: #{employee_age_calculated}"
+
+if employee_age_given > 150
+  employee_age = false
+elsif (employee_age_given != employee_age_calculated) && ( employee_age_given != employee_age_calculated - 1)
+  employee_age = false
+else
+  employee_age = true
+end
+
+puts "Employee age: #{employee_age}"
