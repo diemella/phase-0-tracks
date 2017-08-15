8a6ec8b81e8911a89b029b1282e5b8ebb5f688ac
Diem Hoang <hoangd5@gmail.com>
2017-08-15 16:30:54 -0500

    Build a nested data structure of a highway

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,50 @@
+# Highway
+
+highway = {
+  car_1: {
+    car_info: {
+    car_type: 'van',
+    car_color: 'blue',
+    car_contents: [
+      'vacuum',
+      'car_seat'
+      ]
+    },
+    driver_info: {
+      name: 'Monica',
+      age: 45,
+      occupation: "Chef"
+    }
+  },
+  car_2: {
+    car_info: {
+    car_type: 'truck',
+    car_color: 'silver',
+    car_contents: [
+      'hair_gel',
+      'toy_dinosaurs',
+      'book'
+      ]
+    },
+    driver_info: {
+      name: 'Ross',
+      age: 54,
+      occupation: "Scientist"
+    }
+  },
+  car_3: {
+    car_info: {
+    car_type: 'sedan',
+    car_color: 'yellow',
+    car_contents: [
+      'smelly_cat',
+      'guitar'
+      ]
+    },
+    driver_info: {
+      name: 'Phoebe',
+      age: 36,
+      occupation: "Masseuse"
+    }
+  }
+}
