721ddf224b6e638ad98f64ae936265541785b52c
Diem Hoang <hoangd5@gmail.com>
2017-08-15 17:05:21 -0500

    Show ability to access and change data in nested data structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -7,7 +7,7 @@ highway = {
     car_color: 'blue',
     car_contents: [
       'vacuum',
-      'car_seat'
+      'car seat'
       ]
     },
     driver_info: {
@@ -21,9 +21,9 @@ highway = {
     car_type: 'truck',
     car_color: 'silver',
     car_contents: [
-      'hair_gel',
-      'toy_dinosaurs',
-      'book'
+      'hair gel',
+      'toy dinosaurs',
+      'books'
       ]
     },
     driver_info: {
@@ -37,7 +37,7 @@ highway = {
     car_type: 'sedan',
     car_color: 'yellow',
     car_contents: [
-      'smelly_cat',
+      'smelly cat',
       'guitar'
       ]
     },
@@ -48,3 +48,32 @@ highway = {
     }
   }
 }
+
+##################
+
+p highway[:car_1][:car_info][:car_contents][0]
+# should return item at index 0 (first item) => "vacuum"
+
+highway[:car_1][:car_info][:car_contents].push('box of cookies')
+# add "box of cookies" to the end of the list in car_content of car_1
+
+p highway[:car_1][:car_info][:car_contents].last
+# should return last item on list => "box of cookies"
+
+p highway[:car_2][:car_info][:car_contents][2]
+# should return third item on list => "books"
+
+p highway[:car_2][:car_info][:car_contents].first
+# should return first item on list => "hair gel"
+
+p highway[:car_3][:driver_info][:age]
+# should return => 36
+
+p highway[:car_3][:driver_info][:occupation]
+# should return => "Masseuse"
+
+highway[:car_3][:driver_info][:occupation] = "Singer-Songwriter"
+# change occupation to "Singer-Songwriter" in driver_info of car_3
+
+p highway[:car_3][:driver_info][:occupation]
+# should return => "Singer-Songwriter"
