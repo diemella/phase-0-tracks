4de9af38a6a7ebf3a2de7972818504eaba32729c
Diem Hoang <hoangd5@gmail.com>
2017-08-17 14:55:11 -0500

    Add pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,15 @@
+# PSEUDOCODE
+
+# Create a method that:
+  # takes in a name (string) and turn into two elements (first and last) inside an array, and then swap the order.
+  # split name (new first and last name) into an array of individual letters.
+  # pass each letter through a loop to change letter to next vowel or consonant accordingly
+
+# Create a next_vowel method that takes a vowel and returns the next vowel character
+
+# Create a method that takes a consonant and returns the next consonate character. # consonants = "bcdfghjklmnpqrstvwxyz"
+
+# INPUT -- Agent's Real Name
+# OUTPUT -- Agent's Alias
+# example input: "Felicia Torres"
+# example output: "Vussit Gidmodoe"
