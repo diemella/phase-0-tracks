f643291dbb6956bedc6e49629fb8265d2eaf234f
Diem Hoang <hoangd5@gmail.com>
2017-08-17 14:59:34 -0500

    Create and add methods for alias generator

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,3 +13,76 @@
 # OUTPUT -- Agent's Alias
 # example input: "Felicia Torres"
 # example output: "Vussit Gidmodoe"
+
+
+
+# METHODS
+
+#==========vowels only method==========#
+
+# def next_vowel(word)
+#   vowels = ["a", "e", "i", "o", "u"]
+#   word = word.downcase.chars
+#   word.map! do |letter|
+#     if vowels.include?(letter)
+#     vowels.rotate(1)[vowels.index(letter)]
+#     else
+#       letter
+#     end
+#   end
+#   word.join
+# end
+# # p next_vowel("Felicia Torres")
+
+
+# #==========consonants only method==========#
+
+# def next_consonant(word)
+#   consonants = "bcdfghjklmnpqrstvwxyz"
+#   consonants = consonants.chars
+#   word = word.downcase.chars
+#   word.map! do |letter|
+#     if consonants.include?(letter)
+#     consonants.rotate(1)[consonants.index(letter)]
+#     else
+#       letter
+#     end
+#   end
+#   word.join
+# end
+# # p next_consonant("Zebra")
+
+
+#==========vowels and consonant method==========#
+
+def next_letter (string)
+  consonants = "bcdfghjklmnpqrstvwxyz"
+  consonants = consonants.chars
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  string = string.downcase.chars
+  string.map! do |character|
+    if vowels.include?(character)
+      vowels.rotate(1)[vowels.index(character)]
+    elsif consonants.include?(character)
+      consonants.rotate(1)[consonants.index(character)]
+    else
+      character
+    end
+  end
+  string.join
+end
+# p next_letter("Felicia Torres")
+
+def alias_generator (real_name)
+  real_name = real_name.downcase.split
+  swapped_name = real_name.reverse.join(' ')
+  alias_name = ""
+  char_index = 0
+  while char_index < swapped_name.length
+    alias_name << next_letter(swapped_name[char_index])
+    char_index += 1
+  end
+  alias_name.split.map! { |name| name.capitalize }.join(' ')
+end
+
+# p alias_generator("Felicia Torres") == "Vussit Gimodoe"
