3a2a874ca52795b3009ee41959ca1a2e45ade04c
Diem Hoang <hoangd5@gmail.com>
2017-08-21 15:50:31 -0500

    Build 100 santas of rand gender, ethn, age by accessing attr

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,14 @@
 class Santa
 
-  attr_reader :age, :ethnicity
-  attr_accessor :gender
+  attr_reader :ethnicity, :reindeer_ranking
+  attr_accessor :gender, :age
 
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
+    @age = 0 #rand(1..140)
   end
 
   def speak
@@ -48,14 +48,14 @@ end
 
 # TEST CODE
 
-p santa = Santa.new("male", "Jewish")
-# p santa.speak
-# p santa.eat_milk_and_cookies("snickerdoodle")
-p santa.celebrate_birthday
-p santa.get_mad_at("Vixen")
-p santa.gender=("female")
-p santa.age
-p santa.ethnicity
+# p santa = Santa.new("male", "Jewish")
+# # p santa.speak
+# # p santa.eat_milk_and_cookies("snickerdoodle")
+# p santa.celebrate_birthday
+# p santa.get_mad_at("Vixen")
+# p santa.gender=("female")
+# p santa.age
+# p santa.ethnicity
 
 
 #  Initialization Code from Release:
@@ -96,16 +96,80 @@ p santa.ethnicity
 
 # p santas
 
-santas = []
+# ---------------------------------------------
 
+# santas = []
+
+# genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+# genders.push("transgender", "male")
+# ethnicities.push("Vietnamese", "Irish")
+
+# genders.length.times do |index|
+#   santas << Santa.new(genders[index], ethnicities[index])
+# end
+
+# p santas
+
+
+# Build Many, Many Santas (100)
+
+# declare array of genders
 genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+
+# declare array of ethnicity
 ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
-genders.push("transgender", "male")
-ethnicities.push("Vietnamese", "Irish")
+# create an empty array to hold santas
+santas = []
 
-genders.length.times do |index|
-  santas << Santa.new(genders[index], ethnicities[index])
+# assign 0 to santas_count
+santas_count = 0
+
+# until there are 100 santas,
+  # push randomly selected gender and ethnicity to create an instance of santa into santas array
+  # increment by 1
+  # set new santa's age to a random number between 0 - 140 (move :age to attr_accessor)
+
+until santas_count == 100
+  santas << santa = Santa.new(genders.sample, ethnicities.sample)
+  santa.age = rand(1..140)
+  santas_count += 1
 end
 
 # p santas
+
+# print out the attributes of each santa using the instance methods that you give access to that data
+
+puts "Testing each Santa instance in the array: "
+
+cookies = ["oreo", "snickerdoodle", "chocolate chip", "oatmeal cookie", "sugar cookie"]
+
+reindeers = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
+santas.each do |santa|
+
+  puts "-" * 50
+
+  santa.speak
+  santa.eat_milk_and_cookies(cookies.sample)
+
+  puts "Fun facts about Santa: "
+
+  puts "Gender: #{santa.gender}"
+  puts "Ethnicity: #{santa.ethnicity}"
+
+  puts "Santa's age before celebrating his birthday: #{santa.age}"
+  santa.celebrate_birthday
+  puts "Santa's age after celebrating his birthday: #{santa.age}"
+
+
+  naughty_reindeer = reindeers.sample
+  puts "Santa's reindeer ranking before getting mad at #{naughty_reindeer}: #{santa.reindeer_ranking}"
+  santa.get_mad_at(naughty_reindeer)
+  puts "Santa's reindeer ranking after getting mad at #{naughty_reindeer}: #{santa.reindeer_ranking}"
+
+end
+
+# p santas.count
