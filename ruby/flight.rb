d34aaf7fa0e55d532796fbd1d2ecf03b7e3bc944
Diem Hoang <hoangd5@gmail.com>
2017-08-21 17:50:46 -0500

    Declare Flight module and add take_off method

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,3 +1,13 @@
+# Module Declaration
+
+module Flight
+  def take_off(altitude)
+    puts "Taking off and ascending until reaching #{altitude} ..."
+  end
+end
+
+# Class Declarations
+
 class Bird
   #
 end
