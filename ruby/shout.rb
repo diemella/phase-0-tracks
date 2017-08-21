a3ea09be75f10feadbb88de9883af28c11801394
Diem Hoang <hoangd5@gmail.com>
2017-08-21 18:13:56 -0500

    Include Shout module in Coach and Parent classes. Add test code.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -29,9 +29,19 @@ end
 # Classes Declaration
 
 class Coach
- #
+ include Shout
 end
 
 class Parent
- #
+ include Shout
 end
+
+# Driver Code
+
+coach = Coach.new
+p coach.yell_angrily("Drop and give me 10")
+p coach.yelling_happily("We're going to finals")
+
+parent = Parent.new
+p parent.yell_angrily("Don't talk back to me")
+p parent.yelling_happily("That's my girl")
