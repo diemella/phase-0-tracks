e710b9ce667df6c230df084251392bf75feb4f71
Diem Hoang <hoangd5@gmail.com>
2017-08-21 18:05:38 -0500

    Convert Shout module to a mixin version

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,27 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words + " :)" + " !"
+#   end
+# end
+
+# # DRIVER CODE
+
+# p Shout.yell_angrily("Arg")
+# p Shout.yelling_happily("Yippee")
+
+
+# Module Declaration
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words + " :)" + " !"
   end
 end
-
-# DRIVER CODE
-
-p Shout.yell_angrily("Arg")
-p Shout.yelling_happily("Yippee")
