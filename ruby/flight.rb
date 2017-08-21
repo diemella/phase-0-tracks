0f2ba7ee778fcd60040e0139f48f5d45e4959579
Diem Hoang <hoangd5@gmail.com>
2017-08-21 18:01:35 -0500

    Include Flight module in Bird and Plane classes

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -9,9 +9,17 @@ end
 # Class Declarations
 
 class Bird
-  #
+  include Flight
 end
 
 class Plane
-  #
+  include Flight
 end
+
+# Driver Code
+
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
