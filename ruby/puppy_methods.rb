0ad043f8ce5116bb76058a1b42103f7e974ef383
Diem Hoang <hoangd5@gmail.com>
2017-08-22 10:58:16 -0500

    Add methods and test code to puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,31 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    puts "Woof! " * int
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(int)
+    int * 7
+  end
+
+  def sit
+    puts "*sit*"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
 end
 
+puppy = Puppy.new
+puppy.fetch("ball")
+puppy.speak(3)
+puppy.roll_over
+p puppy.dog_years(2)
+puppy.sit
