80913ee7b2b80cebeaa28192681354f0523b108c
Diem Hoang <hoangd5@gmail.com>
2017-08-28 11:28:46 -0500

    Implement reverse string function and add test code

--- js/explore.js
+++ js/explore.js
@@ -6,3 +6,19 @@
   // add letter to empty string until counter is greater than or equal to 0
   // subtract one from counter
 // return new string
+
+
+// FUNCTION
+
+function reverse(str){
+  var newStr = '';
+  for (var i = str.length-1; i >= 0; i -= 1) {
+    newStr += str[i];
+  }
+  return newStr
+}
+
+// TEST CODE
+
+console.log(reverse('hello'));
+//expect 'olleh'
