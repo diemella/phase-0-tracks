8853d60e4622cf249ea740069a785430a5b0ca1c
Diem Hoang <hoangd5@gmail.com>
2017-08-28 11:27:10 -0500

    Add pseudocode for function to reverse a string

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// PSEUDOCODE
+
+//create an empty string
+//create counter and set it to the length of the string minus 1
+// FOR EACH letter in the string
+  // add letter to empty string until counter is greater than or equal to 0
+  // subtract one from counter
+// return new string
