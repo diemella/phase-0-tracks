a0b898f149195c90af6dda57ad84c78180235347
Diem Hoang <hoangd5@gmail.com>
2017-08-28 19:16:18 -0500

    Add function and driver code for release 0

--- algos.js
+++ algos.js
@@ -10,3 +10,24 @@
     //if word/phrase is longer the current longest, set the word/phrase as current longest.
     // add one to counter
 // return current longest
+
+
+// FUNCTION
+
+function findLongest(arr) {
+  var current_longest = "";
+  current_longest = arr[0];
+  for (var i = 1; i < arr.length; i += 1) {
+  // for (var i = arr.length - 1; i >= 0; i -= 1) {
+    if (arr[i].length > current_longest.length) {
+     current_longest = arr[i];
+    }
+  }
+  return current_longest;
+}
+
+// DRIVER CODE
+
+var test_arr = ["long phrase","word", "longer phrase","longest phrase", "a phrase"];
+
+console.log(findLongest(test_arr));
