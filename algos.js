26bb92e1b2613c0c3a015e4c80cbb9f1fe25bfff
Diem Hoang <hoangd5@gmail.com>
2017-08-28 19:15:03 -0500

    Add pseudocode for finding longest phrase

---
+++ algos.js
@@ -0,0 +1,12 @@
+// Release 0: Find the Longest Phrase
+
+// PSEUDOCODE for Release 0
+
+// create an empty string for current longest
+// set the first word/phrase in list as the current longest
+// create and set counter at the second word/phrase on the list
+//FOR each word/phrase in the list
+  // while counter is less than the number of words/phrases in the list
+    //if word/phrase is longer the current longest, set the word/phrase as current longest.
+    // add one to counter
+// return current longest
