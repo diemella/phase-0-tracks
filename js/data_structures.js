a2a90540e0aba5cdbb7f4e72b02a910ac64457cd
Diem Hoang <hoangd5@gmail.com>
2017-08-30 15:43:32 -0500

    Assign names as keys and colors as values

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,11 @@ var names = ['Seabiscuit', 'Shadow', 'Jolt', 'Thunderbolt'];
 
 colors.push('purple');
 names.push('Dash');
+
+var horses = {}
+
+for (var i = 0; i < names.length; i++) {
+  horses[names[i]] = colors[i];
+}
+
+console.log(horses);
