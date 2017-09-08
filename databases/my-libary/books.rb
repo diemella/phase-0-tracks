c5daf8301e37fd794669fc32010ad671c5de5840
Diem Hoang <hoangd5@gmail.com>
2017-09-08 14:00:33 -0500

    Add method to add books

--- databases/my-libary/books.rb
+++ databases/my-libary/books.rb
@@ -37,7 +37,35 @@ def add_author(db, first_name, last_name)
   end
 end
 
+def add_book(db, title, author_name)
+  titles = db.execute("SELECT title FROM titles")
+  if titles.flatten.include? title
+    puts "#{title} is already on the list!"
+  else
+    authors_ids = db.execute("SELECT * FROM authors")
+
+    ids = {}
+
+    authors_ids.each do |id, first, last|
+      ids[id] = first + " " + last
+    end
+
+    if ids.has_value? author_name
+      author_id = ids.key(author_name)
+        db.execute("INSERT INTO titles (title, author_id) VALUES (?, ?)", [title, author_id])
+    else
+      first_name = author_name.split[0]
+      last_name = author_name.split[1]
+      add_author(db, first_name, last_name)
+      add_book(db, title, author_name)
+    end
+  end
+end
+
 # TEST CODE
 
 # add_author(db, "Jane", "Austen")
 # add_author(db, "Steven", "King")
+
+# add_book(db, "Pride and Prejudice", "Jane Austen")
+# add_book(db, "Kite Runner", "Khaled Hosseini")
